3abde83bccd2eb4456adb2eb9b22d86db72c980a
dtee <93820286+DT2K3@users.noreply.github.com>
2023-12-26 00:00:39 +0700

    update login api

--- Repositories/User/IUserRepository.cs
+++ Repositories/User/IUserRepository.cs
@@ -6,11 +6,12 @@ namespace TEST_CRUD.Repositories
 {
     public interface IUserRepository
     {
-            public Task<User?> GetById(int id);
-            public Task<User?> GetByEmail(string email);
-            public Task<User?> GetByUsername(string username);
-            public Task<User?> Create(RegisterViewModel registerViewModel);
-            public Task<string> Login(LoginViewModel loginViewModel);
-            public Task<bool> ResetPassword(string email, string newPassword);
+        public Task<User?> GetById(int id);
+        public Task<User?> GetByEmail(string email);
+        public Task<User?> GetByUsername(string username);
+        public Task<User?> Create(RegisterViewModel registerViewModel);
+        public Task<(User?, string, int)> Login(LoginViewModel loginViewModel);
+        public Task<bool> ResetPassword(string email, string newPassword);
     }
 }
+
